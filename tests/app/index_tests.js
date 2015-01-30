3d0bd8e949397ac48133986a8723719909f11948
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-01-29 18:47:23 -0600

    Changing behavior!

--- tests/app/index_tests.js
+++ tests/app/index_tests.js
@@ -1,7 +1,7 @@
 describe('app', function() {
   describe('index', function() {
     it('should return hello world', function() {
-      chai.assert.equal('hello world', app.index());
+      chai.assert.equal('hello world in time!', app.index());
     });
   });
 });
